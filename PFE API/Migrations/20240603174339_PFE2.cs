a83aa6e9341bd185ec81b6212f778db320773159
bouammar rachid manyl <bouammar.rachidmanyl@esst-sup.com>
2024-06-04 15:35:49 +0100

    message

---
+++ PFE API/Migrations/20240603174339_PFE2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PFE_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class PFE2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                           name: "rattache",
+                           table: "Equipes",
+                           type: "integer",
+                           nullable: false,
+                           defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+               name: "rattache",
+               table: "Equipes");
+        }
+    }
+}
